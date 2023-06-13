d22c50bd3fa8b4f15c29cd67509459c2067e5bad
Mondestin Myssie <sydneymondestin@gmail.com>
2023-06-13 13:56:12 +0200

    tags

--- main.py
+++ main.py
@@ -14,32 +14,6 @@ tags_metadata = [
     },
 ]
 
-def my_schema():
-   openapi_schema = get_openapi(
-       title="The Amazing Programming Language Info API",
-       version="1.0",
-       routes=app.routes,
-   )
-   openapi_schema["info"] = {
-       "title" : "The Amazing Programming Language Info API",
-       "version" : "1.0",
-       "description" : "Learn about programming language history!",
-       "termsOfService": "http://programming-languages.com/terms/",
-       "contact": {
-           "name": "Get Help with this API",
-           "url": "http://www.programming-languages.com/help",
-           "email": "support@programming-languages.com"
-       },
-       "license": {
-           "name": "Apache 2.0",
-           "url": "https://www.apache.org/licenses/LICENSE-2.0.html"
-       },
-   }
-   app.openapi_schema = openapi_schema
-   return app.openapi_schema
-
-
-
 # Student Model
 class Student(BaseModel):
     id: int
