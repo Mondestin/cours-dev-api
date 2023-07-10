ae7acf8b982e5fddcc1384cc1d295944b920c662
Mondestin Myssie <sydneymondestin@gmail.com>
2023-07-10 19:54:54 +0200

    updates

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from classes.database import database_engine
 import classes.models_orm 
 
 #Routers
-import routers.router_students, routers.router_users, routers.router_classes, routers.router_auth
+import routers.router_students, routers.router_roles ,routers.router_relations , routers.router_users, routers.router_classes, routers.router_auth
 
 # create tables if no presente in the Database
 classes.models_orm.Base.metadata.create_all(bind=database_engine)
@@ -27,4 +27,7 @@ app= FastAPI(
 app.include_router(routers.router_students.router)
 app.include_router(routers.router_users.router)
 app.include_router(routers.router_classes.router)
+app.include_router(routers.router_relations.router)
+app.include_router(routers.router_roles.router)
+
 app.include_router(routers.router_auth.router)
