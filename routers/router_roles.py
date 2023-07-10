09b23b78675515da934d34ecfb5ccba8c57e85d8
Mondestin Myssie <sydneymondestin@gmail.com>
2023-07-10 19:27:42 +0200

    updates

--- routers/router_roles.py
+++ routers/router_roles.py
@@ -5,6 +5,10 @@ from sqlalchemy.orm import Session
 from classes.database import get_cursor
 from classes import models_orm, schemas_dto
 
+from pydantic.typing import Annotated
+from fastapi.security import OAuth2PasswordBearer
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth")
+
 router = APIRouter(
     prefix='/roles',
     tags=['Roles']
@@ -13,6 +17,7 @@ router = APIRouter(
 # get roles
 @router.get('')
 async def get_rolees(
+    token: Annotated[str, Depends(oauth2_scheme)],
     cursor: Session= Depends(get_cursor), 
     limit:int=10, offset:int=0):
     all_roles = cursor.query(models_orm.Roles).limit(limit).offset(offset).all() # Lancement de la requête
@@ -26,7 +31,7 @@ async def get_rolees(
 
 # create role 
 @router.post('', status_code=status.HTTP_201_CREATED)
-async def create_role(payload: schemas_dto.Role_POST_Body, cursor:Session= Depends(get_cursor)):
+async def create_role(token: Annotated[str, Depends(oauth2_scheme)], payload: schemas_dto.Role_POST_Body, cursor:Session= Depends(get_cursor)):
     new_role = models_orm.Roles(name=payload.roleName) # build the insert
     cursor.add(new_role) # Send the query
     cursor.commit() #Save the staged change
@@ -35,7 +40,7 @@ async def create_role(payload: schemas_dto.Role_POST_Body, cursor:Session= Depen
 
 # delete a role  
 @router.delete('/{role_id}', status_code=status.HTTP_204_NO_CONTENT)
-async def delete_role(role_id:int, cursor:Session=Depends(get_cursor)):
+async def delete_role(token: Annotated[str, Depends(oauth2_scheme)], role_id:int, cursor:Session=Depends(get_cursor)):
     # Recherche sur le role existe ? 
     corresponding_role = cursor.query(models_orm.Roles).filter(models_orm.Roles.id == role_id)
     if(corresponding_role.first()):
@@ -51,7 +56,7 @@ async def delete_role(role_id:int, cursor:Session=Depends(get_cursor)):
 
 # Update a role
 @router.patch('/{role_id}')
-async def update_role(role_id: int, payload:schemas_dto.Role_PATCH_Body, cursor:Session=Depends(get_cursor)):
+async def update_role(token: Annotated[str, Depends(oauth2_scheme)], role_id: int, payload:schemas_dto.Role_PATCH_Body, cursor:Session=Depends(get_cursor)):
     # trouver le role correspodant
     corresponding_role = cursor.query(models_orm.Roles).filter(models_orm.Roles.id == role_id)
     if(corresponding_role.first()):
